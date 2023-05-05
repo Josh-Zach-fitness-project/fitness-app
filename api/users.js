94625053b9025fa6fe0a954e8be8cffbdae76f0b
Josh Eardley <josheardley@gmail.com>
2023-05-05 10:03:17 -0600

    friday am commit

--- api/users.js
+++ api/users.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const { getUserByUsername, createUser, getUser, requireUser, getAllRoutinesByUser } = require("../db");
 const jwt = require("jsonwebtoken");
+const { UserTakenError } = require("../errors");
 const usersRouter = express.Router();
 
 // POST /api/users/register
@@ -9,19 +10,21 @@ usersRouter.post("/register", async (req, res, next) => {
     const {username, password} = req.body;
     try {
         const _user = await getUserByUsername(username);
-        // if (_user) {
-        //     next({
-        //         name: "UserExistsError",
-        //         message: "A user by that username already exists"
-        //     });
-        // }
+        if (_user) {
+            next({
+                error: "Error",
+                name: "UserExistsError",
+                message: UserTakenError(username)
+            });
+        }
 
-        // if (password.length < 8) {
-        //     next({
-        //         name: "InvalidPasswordError",
-        //         message: "Password must be at least 8 characters"
-        //     });
-        // }    
+        if (password.length < 8) {
+            next({
+                error: "Error",
+                name: "InvalidPasswordError",
+                message: "Password Too Short!"
+            });
+        }    
 
         const user = await createUser({username, password});
         
@@ -31,8 +34,6 @@ usersRouter.post("/register", async (req, res, next) => {
         }, process.env.JWT_SECRET, {
             expiresIn: "2w"
         })
-        
-        console.log('KKKKKKKK', token)
 
         res.send({
             message: "thank you for registering",
@@ -47,21 +48,23 @@ usersRouter.post("/register", async (req, res, next) => {
 usersRouter.post("/login", async (req, res, next) => {
     const {username, password} = req.body;
     try {
-        const user = await getUser(username, password);
-
+        
         if (!username || !password) {
             next({
-              name: "MissingCredentialsError",
-              message: "Please supply both a username and password"
+                error: "Error",  
+                name: "MissingCredentialsError",
+                message: "Please supply both a username and password"
             });
-          }
+        } else {
+            const user = await getUser({username, password});
+            
+            const token = jwt.veryify(user, process.env.JWT_SECRET);
 
-        const token = jwt.veryify(user, process.env.JWT_SECRET);
+            res.send(["hello"]);
+        }
 
-        res.send({
-        message: "You are logged in",
-        token: token
-        })
+
+        
     } catch (error) {
         next(error);
     }
